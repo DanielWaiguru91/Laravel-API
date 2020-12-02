22af101ec399d313c56a7426d1c86013e5749d56
Daniel Waiguru <danielirungu91@gmail.com>
2020-12-02 19:35:37 +0300

    feat: add method to return all articles

--- app/Http/Controllers/ArticleController.php
+++ app/Http/Controllers/ArticleController.php
@@ -3,10 +3,20 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Article;
 
 class ArticleController extends Controller
 {
     public function index() {
-        
+        return Article::all();
+    }
+    public function store(Request $request) {
+        $validBlog = $request->validate([
+            'uid' => 'required',
+            'title' => 'required|unique:articles|max:255',
+            'content' => 'required'
+        ]);
+        Article::create($validBlog);
+        return $validBlog;
     }
 }
